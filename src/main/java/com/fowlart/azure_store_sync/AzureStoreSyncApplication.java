12d3992550b7bd9fdb82a85342e69b85bb82ee9f
Artur <fowlart1988@gmail.com>
2023-06-28 17:24:38 +0300

    [28.06.2023] added diff download functionality

--- src/main/java/com/fowlart/azure_store_sync/AzureStoreSyncApplication.java
+++ src/main/java/com/fowlart/azure_store_sync/AzureStoreSyncApplication.java
@@ -4,12 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class AzureStoreSyncApplication {
-
 	public static void main(String[] args) {
+		System.out.println(Arrays.toString(args));
 		SpringApplication.run(AzureStoreSyncApplication.class, args);
-
 	}
-
 }
--- src/main/java/com/fowlart/azure_store_sync/CroneCheckConfig.java
+++ src/main/java/com/fowlart/azure_store_sync/CroneCheckConfig.java
@@ -1,9 +1,10 @@
 package com.fowlart.azure_store_sync;
 
+import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.azure.storage.blob.models.BlobItem;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,18 +12,18 @@ import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 import java.io.File;
-import java.io.IOException;
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableScheduling
 public class CroneCheckConfig implements SchedulingConfigurer {
 
-    final
-    Environment env;
+    final Environment env;
 
     public CroneCheckConfig(Environment env) {
         this.env = env;
@@ -34,29 +35,60 @@ public class CroneCheckConfig implements SchedulingConfigurer {
 
     @Override
     public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+
         taskRegistrar.setScheduler(this.taskExecutor());
+
         taskRegistrar.addTriggerTask(() -> {
 
             // 1 -  list all items in container
             var containerName = env.getProperty("azure.storage.container.name");
             var connectionString = env.getProperty("azure.storage.connection.string");
+            var localFolderPath = env.getProperty("local.folder.path");
 
-            BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
-                    .connectionString(Objects.requireNonNull(connectionString))
-                    .buildClient();
+            BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(Objects.requireNonNull(connectionString)).buildClient();
+            BlobContainerClient containerClient = blobServiceClient.createBlobContainerIfNotExists(containerName);
 
-            BlobContainerClient containerClient = blobServiceClient
-                    .createBlobContainerIfNotExists(containerName);
+            var filesInBlob = containerClient.listBlobs().stream().map(BlobItem::getName).collect(Collectors.toSet());
 
-            var itemList = containerClient.listBlobs();
+            // 2 - set up local DIR, sync files between local DIR and blob
+            var localFolder = new File(localFolderPath);
 
-            itemList.forEach(it -> System.out.println(it.getName()));
+            if (!localFolder.exists()) {
+                System.out.println("Attempt to create a folder for synchronization. Folder was created: " + localFolder.mkdirs());
+            }
 
-            // todo: 2 - set up local DIR, sync files between local DIR and blob
-            var localFolder = new File("/Users/artur/IdeaProjects/azure_store_sync/sync");
-            if (!localFolder.exists()) localFolder.mkdirs();
+            var localFiles = Arrays.stream(localFolder.listFiles()).map(File::getName).collect(Collectors.toSet());
 
-            }, triggerContext ->
-                Instant.now().plusSeconds(Integer.parseInt(Objects.requireNonNull(env.getProperty("time.tick")))));
+            var existInBlobButNotInLocal = filesInBlob.stream().filter(bf -> !localFiles.contains(bf)).collect(Collectors.toSet());
+
+            var existInLocalButNotInBlob = localFiles.stream().filter(lf -> !filesInBlob.contains(lf)).collect(Collectors.toSet());
+
+            // if we have some files in blob that need to be downloaded
+            if (!existInBlobButNotInLocal.isEmpty()) {
+
+                existInBlobButNotInLocal.forEach(fname -> {
+                    BlobClient blobClient = containerClient.getBlobClient(fname);
+                    System.out.println("downloading file " + blobClient.getContainerName() + "/" + blobClient.getBlobName());
+                    // todo: here we can work a lot with blob properties
+                    {
+                        var props = blobClient.getProperties();
+                        System.out.println("size: " + props.getBlobSize());
+                        System.out.println("access tier: " + props.getAccessTier());
+                        System.out.println("blob type: " + props.getBlobType().toString());
+                    }
+                    blobClient.downloadToFile(localFolderPath + "/" + fname);
+                });
+            }
+
+            // if we have some files locally that need to be downloaded
+            if (!existInLocalButNotInBlob.isEmpty()) {
+                existInLocalButNotInBlob.forEach(fname -> {
+                    BlobClient blobClient = containerClient.getBlobClient(fname);
+                    System.out.println("uploading file " + blobClient.getContainerName() + "/" + blobClient.getBlobName());
+                    blobClient.uploadFromFile(localFolderPath + "/" + fname);
+                });
+            }
+
+        }, triggerContext -> Instant.now().plusSeconds(Integer.parseInt(Objects.requireNonNull(env.getProperty("time.tick")))));
     }
 }
